4c9f9737e58de2f42e541e238746bfcd72500e96
IonaShanks <Ionashanks@outlook.com>
2016-12-09 23:22:19 +0000

    Made everything look nicer.

---
+++ MovieProject/Controllers/CinemaAdminController.cs
@@ -0,0 +1,133 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using MovieModel;
+using System;
+using System.Linq;
+
+namespace MovieProject.Controllers
+{
+    public class CinemaAdminController : Controller
+    {
+        private MovieContext db = new MovieContext();
+        // GET: CinemaAdmin
+        public ActionResult Index(string title)
+        {
+            string searchString = title;
+            var cinemas = db.Cinemas.Include(c => c.Movies);
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                cinemas = cinemas.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(cinemas);
+        }
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cinema cinema = await db.Cinemas.FindAsync(id);
+            if (cinema == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cinema);
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title");
+            return View();
+        }
+
+        // POST: CinemaAdmin/Add
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Add([Bind(Include = "CinemaID,Name,Website,PhoneNumber,TicketPrice,MovieID")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cinemas.Add(cinema);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title", cinema.MovieID);
+            return View(cinema);
+        }
+
+        // GET: CinemaAdmin/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cinema cinema = await db.Cinemas.FindAsync(id);
+            if (cinema == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title", cinema.MovieID);
+            return View(cinema);
+        }
+
+        // POST: CinemaAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "CinemaID,Name,Website,PhoneNumber,TicketPrice,MovieID")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cinema).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title", cinema.MovieID);
+            return View(cinema);
+        }
+
+        // GET: CinemaAdmin/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cinema cinema = await db.Cinemas.FindAsync(id);
+            if (cinema == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cinema);
+        }
+
+        // POST: CinemaAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Cinema cinema = await db.Cinemas.FindAsync(id);
+            db.Cinemas.Remove(cinema);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MovieProject/Controllers/MovieAdminController.cs
@@ -0,0 +1,147 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using System.Linq;
+using MovieModel;
+using System;
+using System.Collections.Generic;
+
+namespace MovieProject.Controllers
+{
+    public class MovieAdminController : Controller
+    {
+        private MovieContext db = new MovieContext();
+
+        // GET: Movie, filter by genre and title
+        public ActionResult Index(string movieGenre, string searchString)
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre.ToString()
+                           select d.Genre.ToString();
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre.ToString() == movieGenre);
+            }
+
+            return View(movies);
+        }
+        // GET: Movie/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie Movie = await db.Movies.FindAsync(id);
+            if (Movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Movie);
+        }
+
+        // GET: Movie/Add
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: Movie/Add
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Add([Bind(Include = "MovieID,Title,Certification,Genre,Description,RunTime")] Movie Movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(Movie);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(Movie);
+        }
+
+        // GET: Movie/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie Movie = await db.Movies.FindAsync(id);
+            if (Movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Movie);
+        }
+
+        // POST: Movie/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "MovieID,Title,Certification,Genre,Description,RunTime")] Movie Movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Movie).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(Movie);
+        }
+
+        // GET: Movie/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie Movie = await db.Movies.FindAsync(id);
+            if (Movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Movie);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Movie Movie = await db.Movies.FindAsync(id);
+            db.Movies.Remove(Movie);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
